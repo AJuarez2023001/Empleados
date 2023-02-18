6b5a8eefb6bbdf1bfd8ab23fb4f4758669c0699e
AJuarez2023001 <HansAlexanderMerucry@gmail.com>
2023-02-18 12:03:53 -0600

    Ejercicio 1 para registar datos de Empleados con Python

---
+++ Empleado.py
@@ -0,0 +1,12 @@
+#UNA EMPRESA DESEA CONOCER LOS DATOS MAS IMPORTANTES DE LOS POSTULANTES 
+#Se necesita el nombre de la persona edad y numero telefonico
+
+DPI = input("Ingrese el no. DPI                           ")
+NombreCompleto = input("Ingrese Nombre de la Persona      ")
+Edad = int(input("Ingrese la edad de la persona          "))
+Telefono = input("Ingrese el NO. Telefonico               ")
+Direccion =input("Ingres la Direccion de la Persona       ")
+Puesto =input("Ingrese el puesto que solicita             ")
+
+print("  El Postulante A Pide el puesto de:  " +    Puesto + "  Su numero de DPI es:   " +  DPI   + " Su Nombre Completo es:  " 
+      + NombreCompleto +"  Edad:  " + str (Edad) + "  No. de Telefono  " + Telefono + "   Su Direcciòn es : " + Direccion) 
